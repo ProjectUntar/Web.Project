6ac82a53ffb913496a2c6e3c4916d039f94a947f
BrianWij <88884241+BrianWij@users.noreply.github.com>
2021-11-16 09:54:20 +0700

    up

--- Project_WebDev/index.js
+++ Project_WebDev/index.js
@@ -1,13 +1,30 @@
-const express = require('express')
+const express = require('express');
+const bodyParser =require('body-parser');
+const session = require('express-session');
+const layouts = require('express-ejs-layouts');
 
-const app = express()
+const app = express();
 
-app.use(express.static("public"))
-app.set('view engine','ejs')
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded());
+app.use(express.static("public"));
 
-app.get('/',(req,res)=>{
-    res.render('pages/index');
-})
+app.use(session({
+    secret: 'some_secret_key',
+    cookie: {}
+}));
+
+app.use(layouts);
+app.set('layout','layouts/main.ejs');
+
+app.set("layout extractStyles",true)
+
+app.set("layout extractScripts",true)
+
+const index= require('./routes/index');
+
+
+app.get('/',index)
 
 app.listen('3000',()=>{
     console.log('Server sudah bejalan di port 3000')
